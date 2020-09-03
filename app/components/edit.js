99de91df84891183cf88f82b52da56147b0322af
Stas Kolbasin <stas.kolbasin@namecheap.com>
2020-09-03 11:12:15 +0300

    Kind of enabled tracking and saving changes.

--- app/components/edit.js
+++ app/components/edit.js
@@ -3,12 +3,37 @@ import Component from "@glimmer/component";
 import {tracked} from "@glimmer/tracking";
 import { action } from '@ember/object';
 
+class Word {
+  @tracked base;
+
+}
+
 export default class EditComponent extends Component {
 
+  constructor(owner, args) {
+    super(owner, args);
+
+    this.model.base = args.model.base;
+  }
+
+  @tracked model = new Word();
+
+  async getMyJson(){
+    let poco = {};
+    poco.base = this.model.base;
+    return JSON.stringify(poco);
+  }
+
   @action
-  async save(model) {
-    let json = await JSON.stringify(model);
-    let res = await fetch(
+  async showMe() {
+    let json = await this.getMyJson();
+    alert(json);
+  }
+
+  @action
+  async save() {
+    let json = await this.getMyJson();
+    await fetch(
       "https://localhost:5001/word/",
       {
         method: 'POST',
